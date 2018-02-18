8625f8b9ddc05c4480a6c7b26a17704abb2e5000
Adam Gillessen <adamgillessen@gmail.com>
2018-02-18 19:44:08 +0000

    Cleaned up the password check code

--- auth.go
+++ auth.go
@@ -58,18 +58,12 @@ func getUserFromLDAP(username, password string) (user, error) {
 
 	unmarshallDNS(&u, sr.Entries[0].DN)
 
-	if !verifyPassword(password, u, l) {
+	// verify password
+	if err := l.Bind(u.dn, password); err != nil {
 		return u, errWrongPass
 	}
 
 	return u, nil
-}
-
-func verifyPassword(pass string, u user, l *ldap.Conn) bool {
-	if err := l.Bind(u.dn, pass); err != nil {
-		return false
-	}
-	return true
 }
 
 func unmarshallDNS(u *user, dn string) {
