fddb40aa65c09ab03757c8d388d828854b9c1e2d
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2020-03-05 00:26:06 +0000

    Added config loader

--- app/config/config.go
+++ app/config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/Strum355/log"
+
+	"github.com/spf13/viper"
+)
+
+func Load() error {
+	InitDefaults()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	return nil
+}
+
+func PrintSettings() {
+	// Print settings with secrets redacted
+	settings := viper.AllSettings()
+	settings["windlass"].(map[string]interface{})["secret"] = "[redacted]"
+
+	out, _ := json.MarshalIndent(settings, "", "\t")
+	log.Debug("config:\n" + string(out))
+}
